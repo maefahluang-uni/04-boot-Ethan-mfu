01d1a3b0d53305156f65c316ba7b6ffd31342571
Than Htike Zaw <6731503086@lamduan.mfu.ac.th>
2025-09-01 09:32:59 +0000

    Done

--- src/main/java/th/mfu/boot/User.java
+++ src/main/java/th/mfu/boot/User.java
@@ -1,6 +1,41 @@
 package th.mfu.boot;
 
 public class User {
+    private String username;
+    private String displayname;
+    private String email;
+    
+    public User() {}
+
+    public User(String username, String displayname, String email){
+        this.username = username;
+        this.displayname = displayname;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getDisplayname() {
+        return displayname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     
 }
